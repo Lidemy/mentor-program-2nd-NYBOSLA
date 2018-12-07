007417063563aa3bc5a682d11a1b75cbc66ad371
BOSNYLA <shinylincom@gmail.com>
2018-12-07 13:35:13 +0800

    week2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,27 @@
 function alphaSwap(str) {
 
+	let result = '';
+
+	for(var i=0;i<str.length;i++){      
+		if(str.charCodeAt(i)>=65&&str.charCodeAt(i)<=90){          
+			result+=str[i].toLowerCase();       
+		}else if(str.charCodeAt(i)>=97&&str.charCodeAt(i)<=122){        
+			result+=str[i].toUpperCase();       
+		}else{          
+			result+=str[i];       
+		}      
+	}     
+	return result;  
 }
 
+
+// 想要的輸出結果
+// input: nick
+// output: NICK
+
+// input: Nick
+// output: nICK
+
+// input: ,hEllO122
+// output: ,HeLLo123
 module.exports = alphaSwap
--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,20 @@
 function isPalindromes(str) {
+
+	// 建立空陣列
+	var result ='';
+	for(var i=0 ; i<=str.length ;i++){
+		console.log(str)
+		if(str[0])
+		
+	}
   
 }
+
+
+// 結果倒著要一樣
+// abcba => true
+// apple => false
+// aaaaa => true
+// applppa => true
 
 module.exports = isPalindromes
